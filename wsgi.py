134444dad48e4f6ebff18c550a3a001745a1c27b
Brian Leonard <bleonard@hmc.edu>
2015-02-21 10:07:54 -0800

    changed

--- wsgi.py
+++ wsgi.py
@@ -14,6 +14,11 @@ framework.
 
 """
 import os
+import sys
+
+path = '/feed_the_dog/'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feed_the_dog.settings")
 
